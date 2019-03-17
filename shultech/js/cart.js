e327e1ae85a70a5f5dca8a8bf1b9d29168c5aaf0
nitish <nitishkumarlalganj@gmail.com>
2019-03-17 12:57:20 +0530

    cart

--- shultech/js/cart.js
+++ shultech/js/cart.js
@@ -1,23 +1,36 @@
 $(document).ready(function(){
 //continue shopping
 $("#goback").click(function(){
-	parent.history.back();
-		return false;
-	
+  parent.history.back();
+    return false;
+  
 });
 
 
 //remove item from cart
 $(".remove").click(function(){
-
+  $(this).closest(".product_count").remove();
+  product_count();
+  
 });
 //product count
 function product_count(){
   var count = $('.product_count').length;
   $(".p_count").html(count);
+  productsum();
+}
+// total sum
+function productsum()
+{
+  var sum=0;
+  $(".sum").each(function() {
+    sum+=parseInt($(this).text());
+    $("#total_sum").html(sum);
+});
 }
 //product count call
 product_count();
+productsum();
 //place order
 $("#placeorder").click(function(){
 
@@ -25,13 +38,17 @@ $("#placeorder").click(function(){
 
 //icrement
   $('.count').prop('disabled', true);
-   			$(document).on('click','.plus',function(){
-				$('.count').val(parseInt($('.count').val()) + 1 );
-    		});
-        	$(document).on('click','.minus',function(){
-    			$('.count').val(parseInt($('.count').val()) - 1 );
-    				if ($('.count').val() == 0) {
-						$('.count').val(1);
-					}
-    	    	});
+        $(document).on('click','.plus',function(){
+          var myId = $(this).attr('id');
+          var val_b=$("#myId").val();
+
+        $('#'+myId).val(parseInt($('.count').val()) + 1 );
+        });
+          $(document).on('click','.minus',function(){
+             var myid = $(this).attr('id');
+          $('#'+myid).val(parseInt($('#'+myid).val()) - 1 );
+            if ($('.count').val() == 0) {
+            $('.count').val(1);
+          }
+            });
 });
