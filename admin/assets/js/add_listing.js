90d8f146228087628000e983ae409b7e2b12d5a5
Kapil Vishwakarma <kapilvishwakarma26@gmail.com>
2019-04-07 13:06:17 +0530

    Update add_listing.js
    add_listing

--- admin/assets/js/add_listing.js
+++ admin/assets/js/add_listing.js
@@ -1,19 +1,34 @@
 $(document).ready(function (){
 var count=2;
+$("#spec_type").change(function(){
+var a=$("#spec_type").val();
+if(a=='')
+{
+	return;
+}
+else{
+	$("#add_more_div").show();
+	$(".spec_type").val(a);
+}	
+})
 $("#add_more").click(function(){
 var ap_row="";
-
+var b=$("#spec_type").text();
 ap_row+="<div class='row'><div class='col-md-1'><label>#</label><br><span>"+count+"</span></div>";
-    
-    ap_row+=" <div class='col-md-2'><label>Heading</label><select class='form-control'><option>General</option><option>Option2</option> <option>Option 3</option> </select></div>";
-    
-ap_row+=" <div class='col-md-4'><label>Specification</label><input type='text' class='form-control name=''></div>";
+ap_row+=" <div class='col-md-4'><label>Specification</label><input type='text' class='form-control' name=''></div>";
 ap_row+=" <div class='col-md-4'><label>Specification Name</label><input type='text' class='form-control name=''></div>";
 ap_row+="<div class='col-md-1'><label>Action</label><button type='button' class='btn btn-danger  btn-block' id='remove'><span class='fas fa-minus' id='remove'></span></button></div></div>";
 ap_row+="<hr>";
 count++;
 $("#add_more_div .new_row").append(ap_row);
 
+});
+$(".input_file").click(function(){
+$('input[type="file"]').change(function(e){
+	var path=$(this).attr();
+            var fileName = e.target.files[0].name;
+            alert('The file "' + fileName +  '" has been selected.');
+        });
 });
 $('#add_more_div').on('click','#remove',function(){
  $(this).parents(".new_row").remove();
